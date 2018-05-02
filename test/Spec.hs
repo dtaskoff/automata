9adcb09f19058da5ec154efa7f0e03440d857a01
Daniel Taskoff <daniel.taskoff@gmail.com>
2018-05-02 20:40:14 +0300

    Increase the number of quickcheck tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -7,16 +7,19 @@ main :: IO ()
 main = hspec $ do
   describe "FSA.accepts" $ do
     it "returns True if the given automaton accepts the given word" $ do
-      property $ \w -> word w `accepts` w
+      property' $ \w -> word w `accepts` w
     it "returns False if the given automaton doesn't accept the given word" $ do
-      property $ \w w' -> w == w' || not (word w `accepts` w')
+      property' $ \w w' -> w == w' || not (word w `accepts` w')
   describe "FSA.union" $ do
     it "returns the union of two FSA" $ do
-      property $ \w w' ->
+      property' $ \w w' ->
         let fsa = word w `union` word w'
         in  fsa `accepts` w && fsa `accepts` w'
   describe "FSA.concatenate" $ do
     it "returns the concatenation of two FSA" $ do
-      property $ \w w' ->
+      property' $ \w w' ->
         let fsa = word w `concatenate` word w'
         in  fsa `accepts` (w ++ w')
+
+property' :: Testable prop => prop -> Property
+property' = withMaxSuccess 1000 . property
