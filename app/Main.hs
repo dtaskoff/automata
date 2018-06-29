2721afabff9e87beeaf7887d45f29cdaaf4111da
Daniel Taskoff <daniel.taskoff@gmail.com>
2018-06-29 22:26:43 +0300

    Add a small example executable

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,26 @@
 module Main where
 
+import Combinators
+import Example
+import FST
+
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  putStrLn "Transducing with R"
+  print . head . transduce rFST =<< text
+
+  putStrLn "Transducing with R1"
+  print . head . transduce r1FST =<< text
+
+  putStrLn "Conflict freeness of N"
+  print $ conflictFree nFST
+
+  putStrLn "Conflict freeness of N1"
+  print $ conflictFree n1FST
+
+  putStrLn "Conflict freeness of R"
+  print $ conflictFree rFST
+
+  putStrLn "Conflict freeness of R1"
+  print $ conflictFree r1FST
